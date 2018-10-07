6bbce238cb4abb20645515a12fa93588a991aac3
Hari Prakash <hariprakashhp5@gmail.com>
2018-10-07 20:41:55 +0530

    modularized

--- ESP8266_project/esp8266_wifi_controller/callBacks.ino
+++ ESP8266_project/esp8266_wifi_controller/callBacks.ino
@@ -1,10 +1,14 @@
 // Needed for painless library
+
 void receivedCallback( uint32_t from, String &msg ) {
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
   handleReceivedSignal(from, msg);
 }
+
 void newConnectionCallback(uint32_t nodeId) {
     Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
 }
+
 void changedConnectionCallback() {
     Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
 }
